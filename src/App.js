1e54147b68403ae3f9182ca7acdc825b8a553341
MoreAtul <amore@analyticsfoxsoftwares.com>
2023-02-08 17:37:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>Main repo</p>
+        <p>LIC repo</p>
+        <p>Test lic repo</p>
         <a
           className="App-link"
           href="https://reactjs.org"
